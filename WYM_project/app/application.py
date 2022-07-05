383f511116395c4ca1acfc8624c22fa4fa7cda4a
RenaudCrosset <renaud.crosset-perrotin@le-campus-numerique.fr>
2022-07-05 17:09:57 +0200

    suppression des modèles

--- WYM_project/app/application.py
+++ WYM_project/app/application.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # from BDD/connection import User, engine
 
 app = Flask(__name__)				# instantiation application
 
-app.route('/')				        # association d’une route (URL) avec la fonction suivante
-def hello():
-    name1="Renaud"
+@app.route('/about')				        # association d’une route (URL) avec la fonction suivante
+def hello(name1="Renaud"):
     return render_template('index.html', name=name1)
-
-def hello_world():
-    return 'Hello world!'
 
 @app.route('/contact', methods = ['GET','POST'])
 def func_contact():
@@ -22,7 +18,7 @@ def func_contact():
     utilisateur.to_postgres(engine)
     return render_template("contacted.html", name=n, email=e, phone=t, message=m)
 
-hello()
+#hello()
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5010, debug=True)
